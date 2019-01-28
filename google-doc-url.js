cd105c642bc362d96e79a949f0de9e87791922f3
zekunlin <zklin@bu.edu>
2019-01-28 11:33:25 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yT1s4FqfBtsNcTbxDEbflUuAVys-toOcc9guz7AnQ1k/edit#gid=0';
